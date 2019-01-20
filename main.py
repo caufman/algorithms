88bd9a9c7e53c7771230ae05c870d2eda5ded195
caufman <caufman@yandex.ru>
2019-01-20 10:26:08 +0300

    bubbleSort and selectionSort realization

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import sort
 from random import randint
 
 if __name__ == '__main__':
+    arr = [randint(0, 9) for i in range(10)]
+    print("начальный массив:",  arr, "\n")
     print("Пузырьковая сортировка\n\t", 
-        sort.bubbleSort([randint(0, 9) for i in range(10)]), 
+        sort.bubbleSort(arr), 
+        '\n')
+    
+    print("Сортировка вставками\n\t", 
+        sort.selectionSort(arr), 
         '\n')
--- sort.py
+++ sort.py
@@ -6,3 +6,25 @@ def bubbleSort(array):
                 array[j+1] = array[j]
                 array[j] = tmp
     return array
+    
+def selectionSort(array):
+    def minFind(subarray):
+        if len(subarray) > 1:
+            subMin = minFind(subarray[1:])
+            return subarray[0] if subarray[0] < subMin else subMin
+        else:
+            return subarray[0]
+            
+    for i in range(len(array)):
+        ind = array[i:].index(minFind(array[i:]))+i
+        tmp = array[i]
+        array[i] = array[ind]
+        array[ind] = tmp
+    return array
+    
+def insertionSort(array):
+    for i in range(1, len(array)):
+        for j in range(i):
+            while array[i] < array[j]:
+                tmp = array[i]
+                array[i] = 

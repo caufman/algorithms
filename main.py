ee3f3a9bd2f2c6adaa0132605360b3ecbd967fd8
caufman <caufman@yandex.ru>
2019-01-20 07:42:53 +0300

    ***

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ import sort
 from random import randint
 
 if __name__ == '__main__':
-    print(sort.bubbleSort([randint(0, 10) for i in range(10)]))
+    print("Пузырьковая сортировка\n\t", 
+        sort.bubbleSort([randint(0, 9) for i in range(10)]), 
+        '\n')
